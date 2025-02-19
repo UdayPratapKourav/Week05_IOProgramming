fa85d89e5102e40483f8e45724e56907f14baeac
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-19 16:56:43 +0530

    [Uday] Add. filter json data

---
+++ Day02_JSONData/src/main/java/com/jsondata/handsonpracticeproblems/filterjsondata/Person.java
@@ -0,0 +1,11 @@
+package com.jsondata.handsonpracticeproblems.filterjsondata;
+
+public class Person {
+    public String name;
+    public int age;
+    public String email;
+
+    public String toString() {
+        return name + " | Age: " + age + " | Email: " + email;
+    }
+}
